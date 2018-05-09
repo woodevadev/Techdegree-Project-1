dd5967411116704d6cba3d3cbf354b3b8aade208
Alan Wood <woodevadev@gmail.com>
2018-05-08 20:35:34 -0400

    Added quotes

--- js/script.js
+++ js/script.js
@@ -2,3 +2,59 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+var quotes = [
+    {
+        quote: "Don't cry because it's over, smile because it happened.",
+        source: "Dr. Seuss"
+    },
+    {
+        quote: "If you want to know what a man's like, take a doog look at how he treats his inferiors, not his equals.",
+        source: "J.K. Rowling",
+        citation: "Harry Potter and the Goblet of Fire"
+    },
+    {
+        quote: "If you want to know what a man's like, take a doog look at how he treats his inferiors, not his equals.",
+        source: "J.K. Rowling",
+        citation: "Harry Potter and the Goblet of Fire"
+    },
+    {
+        quote: "We welcome change and openness; for we believe that freedom and security go together",
+        source: "Ronald Reagan",
+        citation: "Tear down this wall speech",
+        year: "1987"
+    },
+    {
+        quote: "Two things are infinite: the univers and human stupidity; and im not sure about the universe.",
+        source: "Albert Einstein",
+    },
+    {
+        quote: "So many books, so little time",
+        source: "Frank Zappa",
+    },
+    {
+        quote: "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+        source: "Dr. Seuss",
+    },
+    {
+        quote: "You only live once, but if you it right, once is enough.",
+        source: "Mae West",
+    },
+    {
+        quote: "Be the change that you wish to see in the world.",
+        source: "Mahatma Ghandi",
+    },
+    {
+        quote: "In three words I can sum up everything I've learned about life: it goes on.",
+        source: "Robert Frost",
+    },
+];
+
+
+
+function getRandomQuote{
+
+}
+
+function printQuote{
+
+}
